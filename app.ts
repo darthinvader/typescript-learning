6514784c40bf8bf5a113e54dfc1bf6d48ce29dea
darthinvader <psaradakisj@gmail.com>
2021-09-26 10:36:08 +0300

    [feat] new type string

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
-function add(n1: number, n2: number, printResult: boolean) {
-  if (printResult) console.log(n1 + n2);
-  else return n1 + n2;
+function add(n1: number, n2: number, printResult: boolean, phrase: string) {
+  const result = n1 + n2;
+  if (printResult) console.log(phrase + result);
+  else return result;
 }
 
 const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
-add(number1, number2, printResult);
+const phrase = "result is ";
+add(number1, number2, printResult, phrase);
