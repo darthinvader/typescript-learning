1ac0beae3d28e5768351ed437de8bdbb1d2e11c5
darthinvader <psaradakisj@gmail.com>
2021-09-26 10:34:43 +0300

    [feat] new type

--- app.ts
+++ app.ts
@@ -1,10 +1,9 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number, printResult: boolean) {
+  if (printResult) console.log(n1 + n2);
+  else return n1 + n2;
 }
 
 const number1 = 5;
 const number2 = 2.8;
-
-const sum = add(number1, number2);
-
-console.log(sum);
+const printResult = true;
+add(number1, number2, printResult);
