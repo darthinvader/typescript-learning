ce001cb5d6aa64db08b119ecb2f43ef88964ded6
darthinvader <psaradakisj@gmail.com>
2021-09-27 17:35:57 +0300

    [feat] function type and callback

--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (a: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 let combineValues: (a: number, b: number) => number;
 combineValues = add;
 // combineValues = printResult;
@@ -13,3 +18,8 @@ combineValues = add;
 console.log(combineValues(8, 8));
 
 printResult(add(5, 12));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+  // return result; Can be used
+});
