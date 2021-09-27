44fc3d789d60c7181a7828e3ff06838009af64ea
darthinvader <psaradakisj@gmail.com>
2021-09-27 17:31:53 +0300

    [feat] function type

--- app.ts
+++ app.ts
@@ -4,7 +4,12 @@ function add(n1: number, n2: number): number {
 
 function printResult(num: number): void {
   console.log("Result: " + num);
-  return;
 }
+
+let combineValues: (a: number, b: number) => number;
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(8, 8));
 
 printResult(add(5, 12));
