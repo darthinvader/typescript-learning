0f2d0541cf97b6030f1fb77e4c65f00845c150a1
darthinvader <psaradakisj@gmail.com>
2021-09-27 14:58:24 +0300

    [feat] new array type

--- app.ts
+++ app.ts
@@ -5,6 +5,9 @@
 const person = {
   name: "Maximilian",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["S"];
 console.log(person.name);
