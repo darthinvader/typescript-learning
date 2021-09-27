10c136f56065b6a89d992598a96dcda8e6171902
darthinvader <psaradakisj@gmail.com>
2021-09-27 17:50:21 +0300

    [feat] never type

--- app.ts
+++ app.ts
@@ -4,3 +4,11 @@ let userName: string;
 userInput = 5;
 userInput = "Max";
 if (typeof userInput === "string") userName = userInput;
+
+function generateError(message: string, code: number): never {
+  // throw { message, code };
+  while (true) {}
+}
+
+const result = generateError("An error occured", 500);
+console.log(result);
