73d513b38b425fc7d2997847669b0db89cf66bd8
darthinvader <psaradakisj@gmail.com>
2021-09-27 15:52:59 +0300

    [feat] new enum type loop

--- app.ts
+++ app.ts
@@ -2,16 +2,29 @@
 //   name: string;
 //   age: number;
 // }
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Maximilian",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Maximilian",
   age: 30,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 // person.role.push("admin");
