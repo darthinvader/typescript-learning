6f8813d58ea5b2f5c299d86865f3f6bddfe397ba
darthinvader <psaradakisj@gmail.com>
2021-09-28 19:35:24 +0300

    [feat] Another Generic Function

--- src/app.ts
+++ src/app.ts
@@ -26,3 +26,16 @@ const mergeObj2 = merge<{ name: string }, { age: number }>(
   { age: 30 }
 );
 console.log(mergeObj2.name);
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got No Value";
+  if (element.length > 0) descriptionText = "Got 1 element";
+  if (element.length > 1)
+    descriptionText = "Got " + element.length + " elements";
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hello There"));
