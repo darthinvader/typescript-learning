778a0f43d88bf6ee528841e4c9f75a6111bc7296
darthinvader <psaradakisj@gmail.com>
2021-09-28 16:42:12 +0300

    [feat] interface with classes

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,22 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name: string;
+  age = 30;
+  constructor(n: string) {
+    this.name = n;
+  }
 
-user1 = {
-  name: "Max",
-  age: 30,
   greet(phrase: string) {
     console.log(phrase);
-  },
-};
+  }
+}
 
+let user1: Greetable;
+
+user1 = new Person("Max");
 user1.greet("Hi there");
