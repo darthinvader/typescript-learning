8a4d372fa37bcb63dc8698e4a16ed48846195c49
darthinvader <psaradakisj@gmail.com>
2021-09-28 19:47:57 +0300

    [feat] generic classes

--- src/app.ts
+++ src/app.ts
@@ -48,3 +48,34 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 console.log(extractAndConvert({ name: "Yiannis" }, "name"));
+
+//works only with primatives
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1); //because of this
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Man");
+textStorage.addItem("Mak");
+textStorage.removeItem("Max");
+console.log(textStorage.getItems());
+
+const objStorage = new DataStorage<object>();
+objStorage.addItem({ name: "Max" });
+objStorage.addItem({ name: "Manu" });
+objStorage.removeItem({ name: "Max" });
+
+console.log(objStorage.getItems());
