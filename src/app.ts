a795072155b37432e5836a2513989f909ff5c1c0
darthinvader <psaradakisj@gmail.com>
2021-09-28 19:25:38 +0300

    [feat] creating generic function

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,28 @@
-const names: Array<string> = ["Max", "Manuel"];
+// const names: Array<string> = ["Max", "Manuel"];
 
-names[0].split(" ");
+// names[0].split(" ");
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject("This is not Done!");
-  }, 2001);
-  setTimeout(() => {
-    resolve("This is Done!");
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject("This is not Done!");
+//   }, 2001);
+//   setTimeout(() => {
+//     resolve("This is Done!");
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  console.log(data);
-});
+// promise.then((data) => {
+//   console.log(data);
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+console.log(merge({ name: "Max" }, { age: 30 }));
+const mergeObj = merge({ name: "Max" }, { age: 30 });
+const mergeObj2 = merge<{ name: string }, { age: number }>(
+  { name: "Max" },
+  { age: 30 }
+);
+console.log(mergeObj2.name);
