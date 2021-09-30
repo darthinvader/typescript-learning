2e0480be8adc5f538261e9c18be8bbe991d3e103
darthinvader <psaradakisj@gmail.com>
2021-09-30 21:10:07 +0300

    [feat] working with state and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import TodoList from "./components/TodoList";
+import { Todo } from "./todo.model";
 
 const App: React.FC = () => {
-  const todos = [{ id: "t1", text: "finish the course" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
   const todoAddHandler = (text: string) => {
+    setTodos([{ id: Math.random().toString(), text }]);
     console.log(text);
   };
   return (
