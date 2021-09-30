671cfd8b51ba19d94f90c4a1219ba96491a78dbc
darthinvader <psaradakisj@gmail.com>
2021-09-30 21:11:57 +0300

    [feat] better state management

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ import { Todo } from "./todo.model";
 const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const todoAddHandler = (text: string) => {
-    setTodos([{ id: Math.random().toString(), text }]);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text },
+    ]);
     console.log(text);
   };
   return (
